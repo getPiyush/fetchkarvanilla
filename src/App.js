50972107614c621f83769ff11bc0f3a475c07da9
Piyush Praharaj <piyush.praharaj@outlook.com>
2021-05-10 14:51:04 +0530

    chore : using material-ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import Dashboard from "./components/Dashboard";
+
 export default function App() {
+
     return (
-        <div className="App" >
-            <h1>Taskkar </h1>
-            <span>task sharing app made to ease lifestyle </span>
-        </div>
+        <Dashboard />
     );
 }
